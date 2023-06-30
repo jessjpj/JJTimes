e73b4ea25b8e73314006c14038860e737e04a687
Jeslin Johnson <jeslin@rightfarm.com>
2023-06-30 14:28:22 +0400

    created topstories viewcontroller and topstories view

---
+++ JJTimes/JJTopStoriesView.swift
@@ -0,0 +1,19 @@
+//
+//  JJTopStoriesView.swift
+//  JJTimes
+//
+//  Created by Jeslin Johnson on 30/06/2023.
+//
+
+import UIKit
+
+class JJTopStoriesView: UIView {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
